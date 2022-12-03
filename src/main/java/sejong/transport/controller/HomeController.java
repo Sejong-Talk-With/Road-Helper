1129d3a9e6908bfc3ce37d9997a5519f2447cdbd
potential1205 <potential1205@naver.com>
2022-12-03 20:10:26 +0900

    0.1.1

--- src/main/java/sejong/transport/controller/HomeController.java
+++ src/main/java/sejong/transport/controller/HomeController.java
@@ -1,5 +1,6 @@
 package sejong.transport.controller;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Controller;
@@ -52,6 +53,50 @@ public class HomeController {
         }
 
         return "result";
+    }
+
+    @GetMapping("/detail")
+    public String detailSearch(@ModelAttribute(name = "searchForm") SearchForm searchForm, Model model) {
+
+        Route singleRoutes = new Route("세종대학교 정문","어린이대공원역 1번 출구","근처 엘레베이터를 이용하세요",50,0,10);
+        model.addAttribute("singleRoutes", singleRoutes);
+
+        List<Route> allRoutes = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            Route route = new Route("횡단보도","1번 출구 근처 엘레베이터","예상 시간 5분",50,i,10);
+            allRoutes.add(route);
+        }
+
+        List<Route> detailRoutes = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            Route detail = new Route("횡단보도","1번 출구 근처 엘레베이터","예상 시간 5분",50,i,10);
+            detailRoutes.add(detail);
+        }
+
+        model.addAttribute("allRoutes", allRoutes);
+        model.addAttribute("detailRoutes", detailRoutes);
+        return "detail";
+
+    }
+
+
+    @Data
+    static class Route {
+        public String start;
+        public String end;
+        public String description;
+        public int distance;
+        public int odd;
+        public int time;
+
+        public Route(String start, String end, String description, int distance, int odd, int time) {
+            this.start = start;
+            this.end = end;
+            this.description = description;
+            this.distance = distance;
+            this.odd = odd;
+            this.time = time;
+        }
     }
 
 }
